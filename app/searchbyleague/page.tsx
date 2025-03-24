00255f1b96c114e43804b1bd7135499c86d8a26f
keouryo <zuanamir0511@gmail.com>
2025-03-24 14:58:57 +0300

    changeheader

--- app/searchbyleague/page.tsx
+++ app/searchbyleague/page.tsx
@@ -191,7 +191,7 @@ export default function SearchByLeague() {
 
       <div className="bg-white shadow-md p-4 rounded-lg space-y-4">
         <div className="flex flex-wrap gap-4">
-          =
+          
 
           <Select
             placeholder="Выбрать страну"
